f36c2dc538fd63b8f49b31521f9f5ceaa02c861f
ncip48 <mbahcip00@gmail.com>
2020-02-01 13:21:10 +0700

    Update Main.php

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1,6 +1,19 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Main extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		// Prevent some security threats, per Kevin
+		// Turn on IE8-IE9 XSS prevention tools
+		$this->output->set_header('X-XSS-Protection: 1; mode=block');
+		// Don't allow any pages to be framed - Defends against CSRF
+		$this->output->set_header('X-Frame-Options: DENY');
+		// prevent mime based attacks
+		$this->output->set_header('X-Content-Type-Options: nosniff');		
+	}
+
 	function index(){
 		//redirect(base_url());
 		$data['title'] = title();
