5c07982256600490c7bef890b307ab2f3c803c30
ncip48 <55126134+ncip48@users.noreply.github.com>
2020-02-16 15:04:09 +0700

    Update Artikel.php

--- application/controllers/Artikel.php
+++ application/controllers/Artikel.php
@@ -30,6 +30,34 @@ class Artikel extends CI_Controller {
 		}
 	}
 
+	public function page($hal){
+		$jumlah= $this->model_app->view('berita')->num_rows();
+		$config['base_url'] = base_url().'artikel/index';
+		$config['total_rows'] = $jumlah;
+		$config['per_page'] = 9; 	
+		if ($hal==''){
+			$dari = 0;
+		}else{
+			$dari = $hal;
+		}
+
+		if (is_numeric($dari)) {
+			if ($this->input->post('cari')!=''){
+				$data['title'] = title();
+				$data['judul'] = "Hasil Pencarian keyword - ".filter($this->input->post('cari'));
+				$data['berita'] = $this->model_app->cari_produk(filter($this->input->post('cari')));
+			}else{
+				$data['title'] = title();
+				$data['judul'] = 'Semua Informasi/Artikel';
+				$data['berita'] = $this->model_app->view_ordering_limit('berita','id_berita','DESC',$dari,$config['per_page']);
+				$this->pagination->initialize($config);
+			}
+			$this->template->load('phpmu-one/template','phpmu-one/view_semua_berita',$data);
+		}else{
+			redirect('main');
+		}
+	}
+
 	public function detail($judul){
 		$ids = $judul;
 		//$ids = $this->uri->segment(3);
