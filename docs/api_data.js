809411aa7c32f9652d3aef8fd537b1d0fb66e1df
ncip48 <mbahcip00@gmail.com>
2020-02-17 17:27:16 +0700

    sds

---
+++ docs/api_data.js
@@ -0,0 +1,193 @@
+define({ "api": [
+  {
+    "type": "get",
+    "url": "/kabupaten",
+    "title": "1. Mendapatkan Seluruh Data Kabupaten",
+    "version": "0.1.0",
+    "name": "getKabupaten",
+    "group": "Kabupaten",
+    "permission": [
+      {
+        "name": "public"
+      }
+    ],
+    "description": "<p>digunakan untuk mendapatkan seluruh data kabupaten yang ada di server.</p>",
+    "examples": [
+      {
+        "title": "Cara penggunaan:",
+        "content": "http://iniherly.com/kabupaten",
+        "type": "json"
+      }
+    ],
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "number",
+            "optional": false,
+            "field": "result",
+            "description": "<p>Nomor Hasil.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Object[]",
+            "optional": false,
+            "field": "data",
+            "description": "<p>Daftar data Kabupaten (Array of Objects).</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "data.provinsi_id",
+            "description": "<p>ID Provinsi.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "data.kota_id",
+            "description": "<p>ID Kota/Kabupaten.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "data.nama_provinsi",
+            "description": "<p>Nama Provinsi.</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "data.nama_kabupaten",
+            "description": "<p>Nama Kabupaten/Kota.</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Respon Sukses:",
+          "content": "HTTP/1.1 200 OK\n{\n  result: \"1\",\n  data: [\n        {\n            provinsi_id: \"11\",\n            kota_id: \"1101\",\n            nama_provinsi: \"ACEH\",\n            nama_kabupaten: \"KABUPATEN SIMEULUE\"\n        },\n        {\n            provinsi_id: \"11\",\n            kota_id: \"1102\",\n            nama_provinsi: \"ACEH\",\n            nama_kabupaten: \"KABUPATEN ACEH SINGKIL\"\n        },\n        ...\n        ]\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "./example.js",
+    "groupTitle": "Kabupaten"
+  },
+  {
+    "type": "get",
+    "url": "/provinsi",
+    "title": "1. Mendapatkan Seluruh Data Provinsi",
+    "version": "0.1.0",
+    "name": "getProvinsi",
+    "group": "Provinsi",
+    "permission": [
+      {
+        "name": "public"
+      }
+    ],
+    "description": "<p>digunakan untuk mendapatkan seluruh data provinsi yang ada di server.</p>",
+    "examples": [
+      {
+        "title": "Cara penggunaan:",
+        "content": "http://iniherly.com/provinsi",
+        "type": "json"
+      }
+    ],
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "provinsi_id",
+            "description": "<p>ID Provinsi</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "nama_provinsi",
+            "description": "<p>Nama Provinsi</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Respon Sukses:",
+          "content": "HTTP/1.1 200 OK\n{\n  provinsi_id: \"11\",\n  nama_provinsi: \"ACEH\"\n},\n{\n  provinsi_id: \"12\",\n  nama_provinsi: \"SUMATERA UTARA\"\n}\n...",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "./example.js",
+    "groupTitle": "Provinsi"
+  },
+  {
+    "type": "get",
+    "url": "/provinsi/:provinsi_id",
+    "title": "2. Mendapatkan Detail Data Provinsi",
+    "version": "0.1.0",
+    "name": "getProvinsiDetail",
+    "group": "Provinsi",
+    "permission": [
+      {
+        "name": "public"
+      }
+    ],
+    "description": "<p>digunakan untuk mendapatkan detail provinsi pada server.</p>",
+    "examples": [
+      {
+        "title": "Cara penggunaan:",
+        "content": "http://iniherly.com/provinsi/11",
+        "type": "json"
+      }
+    ],
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "string",
+            "optional": false,
+            "field": "provinsi_id",
+            "description": "<p>id provinsi sesuai dengan record provinsi_id</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "provinsi_id",
+            "description": "<p>ID Provinsi</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "nama_provinsi",
+            "description": "<p>Nama Provinsi</p>"
+          }
+        ]
+      },
+      "examples": [
+        {
+          "title": "Respon Sukses:",
+          "content": "HTTP/1.1 200 OK\n{\n  provinsi_id: \"11\",\n  nama_provinsi: \"ACEH\"\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "./example.js",
+    "groupTitle": "Provinsi"
+  }
+] });
---
+++ docs/api_project.js
@@ -0,0 +1,18 @@
+define({
+  "name": "Dokumentasi API Wilayah Indonesia",
+  "version": "0.1.0",
+  "description": "dokumen api untuk menampilkan provinsi, kabupaten, kecamatan, desa di Indonesia",
+  "apidoc": "0.3.0",
+  "order": [
+    "Provinsi",
+    "Kabupaten"
+  ],
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-02-17T10:11:23.491Z",
+    "url": "http://apidocjs.com",
+    "version": "0.20.0"
+  }
+});
