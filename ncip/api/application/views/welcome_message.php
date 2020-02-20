47f22f58f5d4b3cc6675310cb365fbdd8a1bcfd6
ncip48 <mbahcip00@gmail.com>
2020-02-20 17:02:24 +0700

    Update welcome_message.php

--- ncip/api/application/views/welcome_message.php
+++ ncip/api/application/views/welcome_message.php
@@ -71,27 +71,30 @@
 <body>
 
 <div id="container">
-    <h1>Welcome to CodeIgniter!</h1>
+    <h1>Welcome to Api Wilayah Indonesia</h1>
 
     <div id="body">
-
-        <h2><a href="<?php echo site_url('rest-server'); ?>">REST Server Tests</a></h2>
 
         <?php if (file_exists(FCPATH.'documentation/index.html')) : ?>
         <h2><a href="<?php echo base_url('documentation/index.html'); ?>" target="_blank">REST Server Documentation</a></h2>
         <?php endif ?>
 
-        <p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+        <p>Provinsi :</p>
 
-        <p>If you would like to edit this page you'll find it located at:</p>
-        <code>application/views/welcome_message.php</code>
+        <code><?= base_url() ?>provinsi</code>
 
-        <p>The corresponding controller for this page is found at:</p>
-        <code>application/controllers/Welcome.php</code>
+        <p>Kabupaten :</p>
 
-        <?php if (file_exists(FCPATH.'user_guide/index.html')) : ?>
-        <p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="<?php echo base_url('user_guide/index.html'); ?>" target="_blank">User Guide</a>.</p>
-        <?php endif ?>
+        <code><?= base_url() ?>kabupaten</code>
+
+        <p>Kecamatan :</p>
+
+        <code><?= base_url() ?>kecamatan</code>
+
+        <p>Desa :</p>
+
+        <code><?= base_url() ?>desa</code>
+
     </div>
 
     <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>'.CI_VERSION.'</strong>' : '' ?></p>
